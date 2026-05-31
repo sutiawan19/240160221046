290fc548455985d4000ae22a98621a33b3823eea
240160221034-PutriIndra <240160221034@student.unsap.ac.id>
2026-05-31 22:30:26 +0700

    Modul 04 React State, Event Handling, dan Conditional Rendering

--- A5.240160221034/Belajar-React/kartu-profil-app/src/App.jsx
+++ A5.240160221034/Belajar-React/kartu-profil-app/src/App.jsx
@@ -1,27 +1,17 @@
-import React from 'react';
-import ProfilPengguna from './components/ProfilPengguna';
-import Pengumuman from './components/Pengumuman'; // <-- Impor komponen baru
-import { users } from './data/users';
 import './App.css';
+import TombolAuth from './components/TombolAuth';
 
 function App() {
   return (
-    <div className="app-container">
-      <Pengumuman 
-        judul="Info Praktikum"
-        isi="Pastikan semua tugas dikumpulkan sebelum batas waktu yang ditentukan."
-      />
-      <hr style={{width: '100%', margin: '40px 0', border: '1px solid #eee'}} />
-      <h1>Tim Kami</h1>
-      <div className="profil-list">
-        {users.map(user => (
-          <ProfilPengguna
-            key={user.id}
-            nama={user.nama}
-            pekerjaan={user.pekerjaan}
-            gambar={user.gambar}
-          />
-        ))}
+    <div className="app">
+      <div className="card">
+        <h1>Aplikasi dengan State Persisten</h1>
+
+        <p>
+          Coba klik Login, lalu refresh halaman browser Anda.
+        </p>
+
+        <TombolAuth />
       </div>
     </div>
   );
